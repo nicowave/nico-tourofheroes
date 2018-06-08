819ab84482301dc464bb1186086e4e99c2e929b9
Nicolas Roldos <geoidlife@gmail.com>
2018-06-08 08:32:21 -0700

    added hero-detail component to angular-tour-of-heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -13,7 +14,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
