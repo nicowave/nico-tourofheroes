9f3ecf7184839de06b07d893b5447aeaba74ed3f
Nicolas Roldos <geoidlife@gmail.com>
2018-06-08 08:16:29 -0700

    added ngIf and selectedHero syntax to reflect changes to .ts HeroesComponent changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,7 +5,6 @@ import { Hero } from '../hero';
 import { HEROES } from '../data-model-heroes';
 
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -15,13 +14,20 @@ import { HEROES } from '../data-model-heroes';
 
 
 export class HeroesComponent implements OnInit {
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  } 
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // } 
 
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() { }
 }
