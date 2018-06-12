48a1e6c1c0af59d5118a5704bae4c90cf8b97317
Nicolas Roldos <geoidlife@gmail.com>
2018-06-12 16:21:02 -0700

    css add, router module, routes 'app-routing' module add

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+//  App Routing Module
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+
+@NgModule({
+  imports: 
+    [ CommonModule ],
+  exports: 
+    [ RouterModule ]
+})
+
+export class AppRoutingModule { }
